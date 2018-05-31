e92f6c580d20ad9f374ed6ec5337895bc93a01fa
Jeremy Rempel <jr@idreamincolour.com>
2018-05-30 18:12:37 -1000

    Fix lint error. Manually implement parceable

--- app/src/main/java/app/weatherhistory/android/data/LocationSuggestion.kt
+++ app/src/main/java/app/weatherhistory/android/data/LocationSuggestion.kt
@@ -1,9 +1,30 @@
 package app.weatherhistory.android.data
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
-class LocationSuggestion(private val stationCode: String) : SearchSuggestion {
+class LocationSuggestion(private var stationCode: String) : SearchSuggestion {
+
+    constructor(parcel: Parcel) : this(parcel.readString()) {
+        stationCode = parcel.readString()
+    }
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeString(stationCode)
+    }
+
+    override fun describeContents() = 0
+
     override fun getBody() = stationCode
+
+    companion object CREATOR : Parcelable.Creator<LocationSuggestion> {
+        override fun createFromParcel(parcel: Parcel): LocationSuggestion {
+            return LocationSuggestion(parcel)
+        }
+
+        override fun newArray(size: Int): Array<LocationSuggestion?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
