a4e4660198330b011df3f1098240a914b9c91628
Jeremy Rempel <jr@idreamincolour.com>
2018-05-30 19:25:03 -1000

    Fix a bug from repackaging

--- app/src/main/java/app/weatherhistory/android/search/LocationRepositoryRetrofit.kt
+++ app/src/main/java/app/weatherhistory/android/search/LocationRepositoryRetrofit.kt
@@ -3,10 +3,11 @@ package app.weatherhistory.android.search
 import android.content.Context
 import app.weatherhistory.model.Location
 import io.reactivex.Flowable
+import app.weatherhistory.android.RetrofitUtil
 
 class LocationRepositoryRetrofit(val app: Context) : LocationRepository {
 
-    private val locationService = app.weatherhistory.android.RetrofitUtil.getInstance(app.cacheDir)?.create(LocationService::class.java)
+    private val locationService = RetrofitUtil.getInstance(app.cacheDir)?.create(LocationService::class.java)
 
     override fun findByName(name: String) = locationService?.findByName(name)
             ?: Flowable.empty()
